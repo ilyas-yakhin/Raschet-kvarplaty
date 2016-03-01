e770a62424c6d16be940322e4b25d53b884a58c7
Hazamathebloodedge <hazama@inbox.ru>
2016-03-02 00:37:13 +0500

    Update Kondusov.java

--- models/main/java/ugatu/moodle/raschet_kvarplaty/Kondusov.java
+++ models/main/java/ugatu/moodle/raschet_kvarplaty/Kondusov.java
@@ -4,9 +4,12 @@
 jTextField5.setText(p[1]);jTextField8.setText(p[2]);jTextField11.setText(p[3]);
        jTextField14.setText(p[4]);jTextField17.setText(p[5]);
        //поля с результатами очищаем     
-       jTextField4.setText("");jTextField7.setText("");jTextField10.setText("");
-       jTextField13.setText("");jTextField16.setText("");
+            jTextField13.setText("");jTextField16.setText("");
        jTextField6.requestFocusInWindow();        
+   
+   } catch (Exception ex) {
+     System.out.println(ex.getMessage());
+       
    
    } catch (Exception ex) {
      System.out.println(ex.getMessage());
